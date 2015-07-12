4a0894defaf7d0e1ed243bd9fe8085cb04bd1773
Nicolas Mérouze <nicolas.merouze@gmail.com>
2015-07-12 18:39:32 +0900

    Test promise rejection.

--- test/call.js
+++ test/call.js
@@ -7,20 +7,32 @@ describe('calling function', function() {
 
     fn: function(args) {
       return new Promise(function(resolve, reject) {
-        resolve(args.message);
+        if (args.message === 'ok') {
+          resolve(args.message);
+        } else {
+          reject('ko');
+        }
       });
     }
   };
 
   var s = stack.create(func);
+  // s.configure({ globalOption: null });
 
-  it('should resolve', function(done) {
-    // s.configure({ globalOption: null });
-    
+  it('should be resolved', function(done) {
     stack.run(
       s.use('messageToOutput', { message: 'ok' })
     ).then(function(result) {
-      assert(result, 'ok');
+      assert.equal(result, 'ok');
+      done();
+    })
+  });
+
+  it('should be rejected', function(done) {
+    stack.run(
+      s.use('messageToOutput', { message: 'ko' })
+    ).catch(function(err) {
+      assert.equal(err, 'ko');
       done();
     })
   });
