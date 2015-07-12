45814dcbbb314a75566fe0325e5e537fa762609d
Nicolas Mérouze <nicolas.merouze@gmail.com>
2015-07-12 18:34:03 +0900

    Prepare code to use multiple functions.

--- src/stack.js
+++ src/stack.js
@@ -1,3 +1,16 @@
-module.exports = function(fn) {
-  return fn.fn({});
-}
+module.exports = {
+  create: function(fn) {
+    var use = function(name, args) {
+      return [this.fn[name], args];
+    };
+
+    var obj = {};
+    obj[fn.name] = fn;
+
+    return { fn: obj, use: use };
+  },
+
+  run: function(args) {
+    return args[0].fn(args[1]);
+  }
+};
--- test/call.js
+++ test/call.js
@@ -3,15 +3,23 @@ var stack = require('..');
 
 describe('calling function', function() {
   var func = {
+    name: 'messageToOutput',
+
     fn: function(args) {
       return new Promise(function(resolve, reject) {
-        resolve('ok');
+        resolve(args.message);
       });
     }
   };
 
+  var s = stack.create(func);
+
   it('should resolve', function(done) {
-    stack(func).then(function(result) {
+    // s.configure({ globalOption: null });
+    
+    stack.run(
+      s.use('messageToOutput', { message: 'ok' })
+    ).then(function(result) {
       assert(result, 'ok');
       done();
     })
